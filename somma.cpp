002b7177869e8d813931c09ead7715a3f3a45f7a
Massimo Belfiore <massimobelfiorechitarra@gmail.com>
2022-05-09 00:14:17 +0200

    aggiunta funzione per stampare un messaggio

--- somma.cpp
+++ somma.cpp
@@ -2,22 +2,28 @@
 using namespace std;
 
 int sum(int& , int& );
+void stampaMess();
 
 int main(){
     
     int b = 2;
     int c = 3;
     cout << "la somma di due variabili: "<< sum(b, c) << endl;
+    cout << "il prodotto di due variabili: "<< mult(b, c) << endl;
+    stampaMess();
 
     return 0;
 }
 
 template <typename T>
-T mult(T& a, t& b){
+T mult(T& a, T& b){
     return a * b;
 }
 
 int sum(int& var1, int& var2){
-    int somma = var1 + var2;
-    return somma;
+    return var1 + var2;
+}
+
+void stampaMess(){
+    cout << "Questo sembra un messaggio" << endl;
 }
